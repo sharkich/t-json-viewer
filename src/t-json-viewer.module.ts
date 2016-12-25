95dd6415100b22dddf64e7e4c3fb7baea1b11774
Artem Koziar <a@temich.in.ua>
2016-12-25 13:29:33 +0200

    :fire: fix BrowserModule

--- src/t-json-viewer.module.ts
+++ src/t-json-viewer.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { TJsonViewerComponent } from './t-json-viewer.component/t-json-viewer.component';
 
 @NgModule({
+  imports: [
+    BrowserModule
+  ],
   declarations: [
     TJsonViewerComponent
   ],

cb70b7735fca0d23d6b57504471646bd520dc654
Artem Koziar <a@temich.in.ua>
2017-07-06 23:22:04 +0300

    :fire: v1.1.7 Remove deprecated files, change license

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-export {TJsonViewerModule} from "./src/t-json-viewer.module";
-export {TJsonViewerComponent} from "./src/t-json-viewer.component";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var t_json_viewer_module_1 = require("./src/t-json-viewer.module");
+exports.TJsonViewerModule = t_json_viewer_module_1.TJsonViewerModule;
+var t_json_viewer_component_1 = require("./src/t-json-viewer.component/t-json-viewer.component");
+exports.TJsonViewerComponent = t_json_viewer_component_1.TJsonViewerComponent;
